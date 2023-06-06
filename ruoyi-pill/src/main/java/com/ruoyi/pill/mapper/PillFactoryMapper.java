f72617ab398fcf8e64b10ab79b039884d8883dd1
Arthur_Fans <arthur_fans@163.com>
2023-06-06 18:33:50 +0800

    药品进销存功能

--- ruoyi-pill/src/main/java/com/ruoyi/pill/mapper/PillFactoryMapper.java
+++ ruoyi-pill/src/main/java/com/ruoyi/pill/mapper/PillFactoryMapper.java
@@ -1,15 +1,61 @@
 package com.ruoyi.pill.mapper;
 
+import java.util.List;
 import com.ruoyi.pill.domain.PillFactory;
 
-import java.util.List;
-
 /**
- * @ClassName PillFactoryMapper
- * @Description
- * @Author qinnan fan
- * @Date 2023/6/1 11:01
+ * 生产厂家信息Mapper接口
+ * 
+ * @author qinnan fan
+ * @date 2023-06-02
  */
-public interface PillFactoryMapper {
-    public List<PillFactory> selectPillFactoryList(PillFactory factory);
+public interface PillFactoryMapper 
+{
+    /**
+     * 查询生产厂家信息
+     * 
+     * @param factoryId 生产厂家信息主键
+     * @return 生产厂家信息
+     */
+    public PillFactory selectPillFactoryByFactoryId(Long factoryId);
+
+    /**
+     * 查询生产厂家信息列表
+     * 
+     * @param pillFactory 生产厂家信息
+     * @return 生产厂家信息集合
+     */
+    public List<PillFactory> selectPillFactoryList(PillFactory pillFactory);
+
+    /**
+     * 新增生产厂家信息
+     * 
+     * @param pillFactory 生产厂家信息
+     * @return 结果
+     */
+    public int insertPillFactory(PillFactory pillFactory);
+
+    /**
+     * 修改生产厂家信息
+     * 
+     * @param pillFactory 生产厂家信息
+     * @return 结果
+     */
+    public int updatePillFactory(PillFactory pillFactory);
+
+    /**
+     * 删除生产厂家信息
+     * 
+     * @param factoryId 生产厂家信息主键
+     * @return 结果
+     */
+    public int deletePillFactoryByFactoryId(Long factoryId);
+
+    /**
+     * 批量删除生产厂家信息
+     * 
+     * @param factoryIds 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deletePillFactoryByFactoryIds(Long[] factoryIds);
 }
